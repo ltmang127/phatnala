eda3c190687f849fdd41237707f5fa894b5fc31e
ltmang127 <ltmang127@gmail.com>
2017-08-18 13:22:35 -0700

    Update test.js

--- test.js
+++ test.js
@@ -1,11 +1,9 @@
-<script>
-    alert('here');
-    var app = angular.module("myApp", []);
+alert('here');
+var app = angular.module("myApp", []);
 app.directive("aa", function() {
     return {
         template : "<h1>Made by a directive!</h1>"
     };
 });
+alert('bottom');   
 
-          alert('bottom');   
-</script>
