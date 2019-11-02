37802a0f764a73e732ab4a658765a22851f08245
ltmang127 <ltmang127@gmail.com>
2019-11-01 23:42:21 -0700

    Create search_by_title.js

---
+++ search_by_title.js
@@ -0,0 +1,9 @@
+function searching(){
+var show_value;
+var getValue101=document.getElementById("search111").value;
+if(getValue101[0] == "က"){
+show_value=document.getElementById("show_search").innerHTML="<a href='https://www.myanmarpraiseandworshipsongs.com/p/blog-page_5.html'>က</a>";
+}else{
+show_value=document.getElementById("show_search").innerHTML="no match";
+};
+}
